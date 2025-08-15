9a85d8c830e62736ad50a968a28c371f72fed8b3
Felipe Alafy <git@felipealafy.net>
2025-08-15 08:50:42 -0300

    Added a view for Products and another to Register a product

--- app/src/main/java/net/felipealafy/orgs/ui/theme/Theme.kt
+++ app/src/main/java/net/felipealafy/orgs/ui/theme/Theme.kt
@@ -52,7 +52,7 @@ fun OrgsTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = OrgsTypography,
         content = content
     )
 }
