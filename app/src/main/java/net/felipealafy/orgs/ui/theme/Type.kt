f67fdfb074e85cc2bb0a08f4cb6630698ef1d2cf
Felipe Alafy <git@felipealafy.net>
2025-08-15 10:13:57 -0300

    Added Monsserat font face to the project

--- app/src/main/java/net/felipealafy/orgs/ui/theme/Type.kt
+++ app/src/main/java/net/felipealafy/orgs/ui/theme/Type.kt
@@ -2,35 +2,42 @@ package net.felipealafy.orgs.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import net.felipealafy.orgs.R
 
 // Set of Material typography styles to start with
+val montserratFamily = FontFamily(fonts= listOf(
+    Font(resId = R.font.montserrat_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.montserrat_bold, weight = FontWeight.Bold)
+))
+
 val OrgsTypography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = montserratFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = montserratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = montserratFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = montserratFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 22.sp,
         lineHeight = 24.sp,
